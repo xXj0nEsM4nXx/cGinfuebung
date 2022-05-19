868ace4b8057bcebf37f7bd6a94b83849d80ab9d
MineralLiamG <ldg4sure@gmail.com>
2022-05-19 08:40:19 +0200

    first try movement spaceship

--- uebung.py
+++ uebung.py
@@ -1,9 +1,23 @@
 import pgzrun
+import random
 
 spaceship = Actor('playership1_red')
 gem = Actor('gemblue')
 
+def draw():
+    screen.clear()
+    spaceship.draw()
+    gem.draw()
 
+def on_key_down(key):
+    if key == keys.RIGHT:
+        spaceship.x = +30
+    elif key == keys.LEFT:
+        spaceship.x = -30
+    elif key == keys.UP:
+        spaceship.y = +30
+    elif key == keys.DOWN:
+        spaceship.y = -30
 
 WIDTH = 800
 HEIGHT = 600
